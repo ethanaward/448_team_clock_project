89d03ab40279b48ae5009bf764a3ec413cf22fb2
Rusty Riedel <rusty.riedel@ku.edu>
2016-02-10 11:36:17 -0600

    Add start of InputValidator class.

---
+++ scripts/InputValidator.js
@@ -0,0 +1,20 @@
+var InputValidator = function(){
+
+	this.openEditMenu = function(){
+		//TODO
+		//simply opens the menu with fields for editing time.
+		//either toggles in this function, or when called in the clock class.
+	}
+
+	this.clearInput = function(){
+		//TODO
+		//clears all the input fields for time entry.
+	}
+
+	this.setTime = function(t){
+		//TODO
+		//validates the input is good with given format.
+		//passes valid input to timer obj t.
+		//if input is invalid, send message to display.
+	}
+};
