96e3cdc444a36a9eafd5d454934c1efce000e027
Travis Augustine <augustine@ku.edu>
2016-03-02 08:50:06 -0600

    Add input checking for month and day values based on number of days in the month

--- scripts/InputValidator.js
+++ scripts/InputValidator.js
@@ -37,16 +37,64 @@ var InputValidator = function(){
 		var m = document.getElementById("input_minutes").value;
 		var s = document.getElementById("input_seconds").value;
 		var strmsg = "";
+		var monthtype = 0;
+		if(isNaN(mo)==true||mo<1||mo>12||mo%1!=0 || mo =="") //If months is not a whole number from 1 to 12
+		{
+			strmsg += "\nInvalid months, must be integer between 1 and 12"; 
+		}
+		else if(mo == 1 || mo == 3 || mo == 5 || mo == 7 || mo == 8 || mo == 10 || mo == 12)
+		{
+			monthtype = 31;
+		}
+		else if(mo == 2)
+		{
+			monthtype = 29;
+		}
+		else if(mo == 4 || mo == 6 || mo == 9 || mo == 11)
+		{
+			monthtype = 30;
+		}
+
+		if(monthtype == 31) //if the month has 31 days
+		{
+			if(isNaN(d)==true||d<1||d>31||d%1!=0 || d =="") //If days is not a whole number from 1 to 31
+			{
+				strmsg += "\nInvalid days, must be integer between 1 and 31"; 
+			}
+		}
+		else if(monthtype == 29) //if the month has 29 days
+		{
+			if(isNaN(d)==true||d<1||d>29||d%1!=0 || d =="") //If days is not a whole number from 1 to 29
+			{
+				strmsg += "\nInvalid days, must be integer between 1 and 29"; 
+			}
+		}
+		else if(monthtype == 30) //if the month has 30 days
+		{
+			if(isNaN(d)==true||d<1||d>30||d%1!=0 || d =="") //If days is not a whole number from 1 to 30
+			{
+				strmsg += "\nInvalid days, must be integer between 1 and 30"; 
+			}
+		}
+		else
+		{
+			if(isNaN(d)==true||d<1||d>31||d%1!=0 || d =="") //If days is not a whole number from 1 to 31
+			{
+				strmsg += "\nInvalid days, must be integer between 1 and 31"; 
+			}
+		}
 
 		if(isNaN(s)==true||s<0||s>59||s%1!=0) //If sec is not a whole number from 0 to 59
 		{
-			
-			strmsg += "Invalid seconds, must be integer between 0 and 59";
+			strmsg += "\nInvalid seconds, must be integer between 0 and 59";
 		}
 		if(isNaN(m)==true||m<0||m>59||m%1!=0) //If min is not a whole number from 0 to 59
 		{
 			strmsg += "\nInvalid minutes, must be integer between 0 and 59"; 
 		}
+
+
+
 		if((document.getElementById("u_24H").checked == true)&&(isNaN(h)==true||h<0||h>23||h%1!=0)) //If hour is not a whole number from 0 to 23 for 24 hour format
 		{
 			strmsg += "\nInvalid hours, must be integer between 0 and 23";
@@ -57,7 +105,7 @@ var InputValidator = function(){
 		}		
 
 		else 
-	 	{	if(m>=0 && m<60 && isNaN(m)==false && m%1==0 && s>=0 && s<60 && isNaN(s)==false && s%1==0) // necessary
+	 	{	if(m>=0 && m<60 && isNaN(m)==false && m%1==0 && s>=0 && s<60 && isNaN(s)==false && s%1==0 && isNaN(mo)==false && mo%1==0 && mo != "" && mo>0 && mo <13 && isNaN(d)==false && d%1==0 && d!="" && d>0 && d<32) // necessary
 			{
 	 			//input was valid so set the time in the timer object.
 				if(document.getElementById("pm").checked == true)
