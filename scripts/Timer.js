6931779e047a6b0652e4ddc4bde2f481b4fbb511
Travis Augustine <augustine@ku.edu>
2016-03-02 09:21:39 -0600

    Make clock roll over to next day when it hits 24 hours, and make clock roll over to next month when it hits appropriate final date

--- scripts/Timer.js
+++ scripts/Timer.js
@@ -26,6 +26,7 @@ var Timer = function(pMonth, pDay, pHours, pMin, pSec, pFormat){
 	var format = pFormat;
 	var sec_total = 0;
 	var meridiem = "AM";
+	var firstRun = true;
 
 	//methods---------------------------------------------------------
 	/**
@@ -64,13 +65,43 @@ var Timer = function(pMonth, pDay, pHours, pMin, pSec, pFormat){
 		}
 		
 		//time increment logic
-		if(sec_total < 86400 -1) // make clock restart at 24:00 hr -- instead of displaying 24:00 show 0:00
+		if(sec_total < 86400 - 1) // make clock restart at 24:00 hr -- instead of displaying 24:00 show 0:00
 	 	{
 	 		sec_total += 1; //Increase time by 1 sec
-	 	}
-	 	else 
+
+			if(sec_total == 1 && firstRun == false)
+			{
+				if((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10) && day == 31)
+				{
+					day = 0;
+					month = Number(month) +1;
+				}
+				else if(month == 12 && day == 31)
+				{
+					day = 0;
+					month = 1;
+				}
+				else if((month == 4 || month == 6 || month == 9 || month == 11) && day == 30)
+				{
+					day = 0;
+					month = Number(month)+1;
+				}
+				else if(month == 2 && day == 29)
+				{
+					day = 0;
+					month = 3;
+				}
+				else
+				{
+					day = Number(day) +1;
+				}
+			}
+
+		}
+	 	else
 		{
 	 		sec_total = 0;
+			firstRun = false;
 		}	
 	}
 
