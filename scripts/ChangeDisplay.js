dae53467d847c40b05c8441e223e3806cab8f38a
Travis Augustine <augustine@ku.edu>
2016-03-04 11:53:35 -0600

    Add functionality for hiding and showing timer and stopwatch forms

--- scripts/ChangeDisplay.js
+++ scripts/ChangeDisplay.js
@@ -4,9 +4,18 @@ function display_timer() {
 		var stopwatch = document.getElementById('stopwatch_display');
 		var timer = document.getElementById('timer_display');
 
+		var clock_form = document.getElementById('edit_time_div');
+		var timer_form = document.getElementById('edit_timer');
+		var stopwatch_form = document.getElementById('edit_stopwatch');
+
 		clock.style.display = 'none';
 		stopwatch.style.display = 'none';
 		timer.style.display = 'block';
+
+		clock_form.style.display = 'none';
+		timer_form.style.display = 'block';
+		stopwatch_form.style.display = 'none';
+		
 		
 	
 	}
@@ -17,9 +26,17 @@ function display_timer() {
 		var stopwatch = document.getElementById('stopwatch_display');
 		var timer = document.getElementById('timer_display');
 
+		var clock_form = document.getElementById('edit_time_div');
+		var timer_form = document.getElementById('edit_timer');
+		var stopwatch_form = document.getElementById('edit_stopwatch');
+
 		clock.style.display = 'block';
 		stopwatch.style.display = 'none';
 		timer.style.display = 'none';
+
+		clock_form.style.display = 'block';
+		timer_form.style.display = 'none';
+		stopwatch_form.style.display = 'none';
 		
 	}
 
@@ -29,9 +46,18 @@ function display_timer() {
 		var stopwatch = document.getElementById('stopwatch_display');
 		var timer = document.getElementById('timer_display');
 
+
+		var clock_form = document.getElementById('edit_time_div');
+		var timer_form = document.getElementById('edit_timer');
+		var stopwatch_form = document.getElementById('edit_stopwatch');
+
 		clock.style.display = 'none';
 		stopwatch.style.display = 'block';
 		timer.style.display = 'none';
+
+		clock_form.style.display = 'none';
+		timer_form.style.display = 'none';
+		stopwatch_form.style.display = 'block';
 		
 	
 	}
