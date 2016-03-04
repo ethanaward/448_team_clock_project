c8c64d360f38c584bf5e13f345b6c31b331b385a
NeisesMike <neisesmichael@gmail.com>
2016-03-04 13:12:30 -0600

    Refactor NewTimer
    Use millisecond calls instead of entire date calls

--- scripts/NewTimer.js
+++ scripts/NewTimer.js
@@ -5,13 +5,11 @@ function MakeTimer() {
 	var hours = document.getElementById("timer_hours").value;
 	var min = document.getElementById("timer_mins").value;
 	var sec = document.getElementById("timer_secs").value;
-	// alert(hours);
 
-	var curDate = new Date();
-	timeDate = new Date (2016, 3, 3, curDate.getHours() + hours, curDate.getMinutes() + min, curDate.getSeconds() + sec);
-	var diff = timeDate - curDate;
-	var test = new Date(diff);
-	var display = test.getHours() + ":" + test.getMinutes() + ":" + test.getSeconds();
+	timeDate = new Date();
+	startTime = (hours-18) * 3600000 + min * 60000 + sec * 1000;
+	var originalDisplay = new Date(startTime);
+	var display = originalDisplay.getHours() + ":" + originalDisplay.getMinutes() + ":" + originalDisplay.getSeconds();
 	document.getElementById("timer").innerHTML = display;
 	window.setInterval(RunTimer, 1000);
 	
@@ -19,13 +17,12 @@ function MakeTimer() {
 
 function RunTimer() {
 	var newDate = new Date();
-	var newDiff = timeDate - newDate;
-	// alert(newDiff);
-	var test2 = new Date(newDiff);
-	var display2 = test2.getHours() + ":" + test2.getMinutes() + ":" + test2.getSeconds();
-	document.getElementById("timer").innerHTML = display2;
+	var newDiff = newDate.getTime() - timeDate.getTime();
+	var newTime = startTime - newDiff;
+	var newDisplay = new Date(newTime);
+	var countDown = newDisplay.getHours() + ":" + newDisplay.getMinutes() + ":" + newDisplay.getSeconds();
+	document.getElementById("timer").innerHTML = countDown;
 	
 
 
 }
-
