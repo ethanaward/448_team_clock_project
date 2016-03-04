8c70a2c5ba7f5faf899b19be3364d3f0a26ce306
NeisesMike <neisesmichael@gmail.com>
2016-03-04 12:20:41 -0600

    Condense three functions into one

--- scripts/ChangeDisplay.js
+++ scripts/ChangeDisplay.js
@@ -1,64 +1,32 @@
-function display_timer() {
+function display(num) {
 
 		var clock = document.getElementById('clock_display');
-		var stopwatch = document.getElementById('stopwatch_display');
 		var timer = document.getElementById('timer_display');
+		var stopwatch = document.getElementById('stopwatch_display');
 
 		var clock_form = document.getElementById('edit_time_div');
 		var timer_form = document.getElementById('edit_timer');
-		var stopwatch_form = document.getElementById('edit_stopwatch');
+		var stopwatch_form = document.getElementById('edit_stopwatch');	
 
 		clock.style.display = 'none';
 		stopwatch.style.display = 'none';
-		timer.style.display = 'block';
+		timer.style.display = 'none';
 
 		clock_form.style.display = 'none';
-		timer_form.style.display = 'block';
+		timer_form.style.display = 'none';
 		stopwatch_form.style.display = 'none';
-		
-		
-	
-	}
 
-	function display_clock() {
-
-		var clock = document.getElementById('clock_display');
-		var stopwatch = document.getElementById('stopwatch_display');
-		var timer = document.getElementById('timer_display');
-
-		var clock_form = document.getElementById('edit_time_div');
-		var timer_form = document.getElementById('edit_timer');
-		var stopwatch_form = document.getElementById('edit_stopwatch');
-
+	if(num==0){
 		clock.style.display = 'block';
-		stopwatch.style.display = 'none';
-		timer.style.display = 'none';
-
 		clock_form.style.display = 'block';
-		timer_form.style.display = 'none';
-		stopwatch_form.style.display = 'none';
-		
 	}
-
-	function display_stopwatch() {
-
-		var clock = document.getElementById('clock_display');
-		var stopwatch = document.getElementById('stopwatch_display');
-		var timer = document.getElementById('timer_display');
-
-
-		var clock_form = document.getElementById('edit_time_div');
-		var timer_form = document.getElementById('edit_timer');
-		var stopwatch_form = document.getElementById('edit_stopwatch');
-
-		clock.style.display = 'none';
+	if(num==1){
+		timer.style.display = 'block';
+		timer_form.style.display = 'block';
+	}
+	if(num==2){
 		stopwatch.style.display = 'block';
-		timer.style.display = 'none';
-
-		clock_form.style.display = 'none';
-		timer_form.style.display = 'none';
 		stopwatch_form.style.display = 'block';
-		
-	
 	}
-
+	
+}
