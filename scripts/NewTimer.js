072b10bdf50fa7cb35d7ce0c1dccc0fe7f7e880f
Ethan Ward <ethanandrewward@gmail.com>
2016-03-04 12:38:11 -0600

    Update timer to display numbers

--- scripts/NewTimer.js
+++ scripts/NewTimer.js
@@ -5,12 +5,14 @@ function MakeTimer() {
 	var hours = document.getElementById("timer_hours").value;
 	var min = document.getElementById("timer_mins").value;
 	var sec = document.getElementById("timer_secs").value;
-	alert(hours);
+	// alert(hours);
 
 	var curDate = new Date();
 	timeDate = new Date (2016, 3, 3, curDate.getHours() + hours, curDate.getMinutes() + min, curDate.getSeconds() + sec);
 	var diff = timeDate - curDate;
-	document.getElementById("timer_display").firstChild.nodeValue = diff;
+	var test = new Date(diff);
+	var display = test.getHours() + ":" + test.getMinutes() + ":" + test.getSeconds();
+	document.getElementById("timer_display").firstChild.nodeValue = display;
 	window.setInterval(RunTimer, 1000);
 	
 }
@@ -19,7 +21,9 @@ function RunTimer() {
 	var newDate = new Date();
 	var newDiff = timeDate - newDate;
 	// alert(newDiff);
-	document.getElementById("timer_display").firstChild.nodeValue = newDiff;
+	var test2 = new Date(newDiff);
+	var display2 = test2.getHours() + ":" + test2.getMinutes() + ":" + test2.getSeconds();
+	document.getElementById("timer_display").firstChild.nodeValue = display2;
 	
 
 
