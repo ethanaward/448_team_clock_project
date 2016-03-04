835849580014da57b959a973c5050d6379b1ac3f
Travis Augustine <augustine@ku.edu>
2016-03-03 20:04:36 -0600

    implement buttons to switch between clock, timer and stopwatch

---
+++ scripts/ChangeDisplay.js
@@ -0,0 +1,38 @@
+function display_timer() {
+
+		var clock = document.getElementById('clock_display');
+		var stopwatch = document.getElementById('stopwatch_display');
+		var timer = document.getElementById('timer_display');
+
+		clock.style.display = 'none';
+		stopwatch.style.display = 'none';
+		timer.style.display = 'block';
+		
+	
+	}
+
+	function display_clock() {
+
+		var clock = document.getElementById('clock_display');
+		var stopwatch = document.getElementById('stopwatch_display');
+		var timer = document.getElementById('timer_display');
+
+		clock.style.display = 'block';
+		stopwatch.style.display = 'none';
+		timer.style.display = 'none';
+		
+	}
+
+	function display_stopwatch() {
+
+		var clock = document.getElementById('clock_display');
+		var stopwatch = document.getElementById('stopwatch_display');
+		var timer = document.getElementById('timer_display');
+
+		clock.style.display = 'none';
+		stopwatch.style.display = 'block';
+		timer.style.display = 'none';
+		
+	
+	}
+
