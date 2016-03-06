54e2373636baf286533e8eab9dbff3a6518c33b8
NeisesMike <neisesmichael@gmail.com>
2016-03-06 03:49:50 -0600

    Add stopping functionality. Fix hours-to-days conversion.

--- scripts/NewTimer.js
+++ scripts/NewTimer.js
@@ -1,12 +1,15 @@
 var timeDate;
 var stop = false;
 var pause = false;
+var daysOver = 0;
 
 function MakeTimer() {
 
 	var hours = document.getElementById("timer_hours").value;
 	var min = document.getElementById("timer_mins").value;
 	var sec = document.getElementById("timer_secs").value;
+
+	daysOver = (hours - (hours % 24))/24;
 
 	stop = false;
 
@@ -77,9 +80,15 @@ function OutputTimer(countDate){
 
 	var seconds = (countDate.getSeconds() < 10 ? "0" : "") + countDate.getSeconds();
 	var minutes = (countDate.getMinutes() < 10 ? "0" : "") + countDate.getMinutes();
-	var hours = (countDate.getHours() < 10 ? "0" : "") + countDate.getHours();
+	var hours = ((countDate.getHours()+24*daysOver) < 10 ? "0" : "") + (countDate.getHours()+24*daysOver);
 
 	var countDown = hours + ":" + minutes + ":" + seconds;
+
+	if(seconds==0&&minutes==0&&hours==0){
+		stop = true;
+		document.getElementById("timer").innerHTML = countDown;
+		alert("Timer is finished!");
+	}
 
 	document.getElementById("timer").innerHTML = countDown;
 }
