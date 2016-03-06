b82b2f942f9da6de05406c547d49c9c985b0279a
NeisesMike <neisesmichael@gmail.com>
2016-03-06 14:59:09 -0600

    Add dayOfWeek calculations and output

--- scripts/Display.js
+++ scripts/Display.js
@@ -16,7 +16,55 @@ var Display = function(){
 	 * the Timer class with indicies [hour, min, sec, meridiem].
 	 */
 	this.displayTime = function(timeArr){
+		var daysPassed = 0;
+		var weekDay = 0;
+		var week = ["Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"]
+
 		document.getElementById("time").innerHTML = this.checkNumber(timeArr[4]) + "/" + this.checkNumber(timeArr[5]) + " " + this.checkNumber(timeArr[0]) + ":" + this.checkNumber(timeArr[1]) + ":" + this.checkNumber(timeArr[2]) + " " + timeArr[3];
+
+
+		if(timeArr[4] == 1){
+			daysPassed = timeArr[5] - 1;
+		}
+		if(timeArr[4] == 2){
+			daysPassed = timeArr[5] + 31 - 2;
+		}
+		if(timeArr[4] == 3){
+			daysPassed = timeArr[5] + 60 - 2;
+		}
+		if(timeArr[4] == 4){
+			daysPassed = timeArr[5] + 91 - 2;
+		}
+		if(timeArr[4] == 5){
+			daysPassed = timeArr[5] + 121 - 2;
+		}
+		if(timeArr[4] == 6){O
+			daysPassed = timeArr[5] + 152 - 2;
+		}
+		if(timeArr[4] == 7){
+			daysPassed = timeArr[5] + 182 - 2;
+		}
+		if(timeArr[4] == 8){
+			daysPassed = timeArr[5] + 213 - 2;
+		}
+		if(timeArr[4] == 9){
+			daysPassed = timeArr[5] + 244 - 2;
+		}
+		if(timeArr[4] == 10){
+			daysPassed = timeArr[5] + 274 - 2;
+		}
+		if(timeArr[4] == 11){
+			daysPassed = timeArr[5] + 305 - 2;
+		}
+		if(timeArr[4] == 12){
+			daysPassed = timeArr[5] + 335 - 2;
+		}
+
+		weekDay = daysPassed % 7;
+
+
+		document.getElementById("dayOfWeek").innerHTML = week[weekDay];
+	
 	}
 
 	/**
